141b3f38f6b62a79e906d27430adfa336d0553d1
Errol <errolflynn8@hotmail.com>
2020-07-24 00:09:04 +1000

    Cleanup of the pacman script

--- Pacman/Assets/Scripts/Pacman.cs
+++ Pacman/Assets/Scripts/Pacman.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.Serialization.Json;
 using UnityEngine;
-using UnityEngine.Rendering;
 
+/// <summary>
+/// A class that houses all the behaivour for pacman
+/// </summary>
 public class Pacman : MonoBehaviour
 {
     public enum State
@@ -24,7 +25,6 @@ public class Pacman : MonoBehaviour
     Vector2 nextDirection;
 
     // values to keep track of the movement from the current to the target node
-    float progress01 = 0;
     float distanceFromCurrent = 0;
     float distanceToTarget = 0;
 
@@ -32,16 +32,19 @@ public class Pacman : MonoBehaviour
     public State currentState;
     public Vector2 currentPos;
 
-    public void Init(Node node)
+    /// <summary>
+    /// Sets up pacman
+    /// </summary>
+    /// <param name="startingNode">The node to start at</param>
+    public void Init(Node startingNode)
     {
         trans = transform;
 
-        currentPos = node.pos;
+        currentNode = startingNode;
+        currentPos = currentNode.pos;
         trans.position = currentPos;
 
         currentState = State.idle;
-        currentNode = node;
-
         currentDirection = Vector2.zero;
     }
 
@@ -49,28 +52,39 @@ public class Pacman : MonoBehaviour
     {
         HandleInput();
 
-        // if we are moving, move
+        // if we are moving, do the move stuff
         if (currentState == State.moving)
         {
-            distanceFromCurrent += moveSpeed * Time.deltaTime;                          // move based on the movespeed
-            progress01 = Mathf.InverseLerp(0, distanceToTarget, distanceFromCurrent);   // calculate the normalised progress to the next node
-            currentPos = Vector2.Lerp(currentNode.pos, targetNode.pos, progress01);     // get the current position
-
-            trans.position = currentPos;                                                // set the current position
+            distanceFromCurrent += moveSpeed * Time.deltaTime;                                  // move based on the movespeed
+            float progress01 = Mathf.InverseLerp(0, distanceToTarget, distanceFromCurrent);     // calculate the normalised progress to the next node
+            currentPos = Vector2.Lerp(currentNode.pos, targetNode.pos, progress01);             // get the current position
+            trans.position = currentPos;                                                        // set the current position
 
             // if we have reached the target node, try to keep moving
             if (progress01 == 1) 
             {
-                currentState = State.idle;
+
+                // TODO: collect any pellets/powerups/whatever
 
                 currentNode = targetNode;
-                bool canMove = Move();
+                bool canMove = MoveFromNode();
+                if (!canMove)
+                    currentState = State.idle;
             }
         }
     }
-
-    void StartMove(Node a, Node b, Vector2 direction, float currentDist = 0)
+    
+    /// <summary>
+    /// Sets up and starts pacman moving from 1 node to another
+    /// </summary>
+    /// <param name="a">The node we are moving from</param>
+    /// <param name="b">The node we are moving to</param>
+    /// <param name="direction">The direction we are moving</param>
+    /// <param name="startingDist">An optional paramater to specify pacmans starting distance from a to b</param>
+    void StartMove(Node a, Node b, Vector2 direction, float startingDist = 0)
     {
+        // TODO: animation triggers
+
         currentState = State.moving;
 
         currentDirection = direction;
@@ -78,20 +92,24 @@ public class Pacman : MonoBehaviour
         currentNode = a;
         targetNode = b;
 
-        distanceToTarget = Vector2.Distance(currentNode.pos, targetNode.pos);
-        distanceFromCurrent = currentDist;
-        progress01 = Mathf.InverseLerp(0, distanceToTarget, distanceFromCurrent);
+        distanceToTarget = Vector2.Distance(currentNode.pos, targetNode.pos);       // first calculate the distance required
+        distanceFromCurrent = startingDist;
     }
 
-    bool Move()
+    /// <summary>
+    /// Tries to move pacman from a node (when he has just reached a node or is idle)
+    /// </summary>
+    /// <returns>If we can move or not</returns>
+    bool MoveFromNode()
     {
+        // first see if we can move in the next/cached direction
         Node nextNode = CanMove(nextDirection);
         if (nextNode != null)
         {
             StartMove(currentNode, nextNode, nextDirection);
             return true;
         }
-        else
+        else if (currentState != State.idle) // now check the current direction if we arent idle (if we are idle the directions must be the same)
         {
             nextNode = CanMove(currentDirection);
             if (nextNode != null)
@@ -104,30 +122,32 @@ public class Pacman : MonoBehaviour
         return false;
     }
 
+    /// <summary>
+    /// Try to move pacman from the input
+    /// </summary>
+    /// <returns>If we can move or not</returns>
     bool MoveInput()
     {
+        // if we are moving, check only for going in opposite direction, if not return false
         if (currentState == State.moving)
         {
             // we are going in the opposite direction
             if (Vector2.Dot(nextDirection, currentDirection) == -1)
             {
-                StartMove(targetNode, currentNode, nextDirection, distanceToTarget - distanceFromCurrent);
+                StartMove(targetNode, currentNode, nextDirection, distanceToTarget - distanceFromCurrent); // move the opposite direction, making sure we reverse the progress
                 return true;
             }
-            return false;
+            return false; // not going in opposite direction, dont do anything
         }
 
-        Node nextNode = CanMove(nextDirection);
-        if (nextNode != null)
-        {
-            StartMove(currentNode, nextNode, nextDirection);
-            return true;
-        }
-
-        return false;
-        
+        // we are still here, set both directions as the same and try to move the normal way
+        currentDirection = nextDirection;
+        return MoveFromNode();
     }
     
+    /// <summary>
+    /// Handles input (duh)
+    /// </summary>
     void HandleInput()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
@@ -152,6 +172,12 @@ public class Pacman : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// If we are able to move in a given direction
+    /// NOTE: this only works if we are idle (because it uses the current node and pacman could be inbetween nodes)
+    /// </summary>
+    /// <param name="direction">The direction to query</param>
+    /// <returns>Returns a node if we can move in the given direction, returns null if we cant move</returns>
     Node CanMove(Vector2 direction)
     {
         for (int i = 0; i < currentNode.neighbours.Length; i++)
