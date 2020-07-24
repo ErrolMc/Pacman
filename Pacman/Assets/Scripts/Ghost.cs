8e74f5da402f22db560eab5c3879eb4af07ba7e2
Errol <errolflynn8@hotmail.com>
2020-07-25 00:43:49 +1000

    EOD ghost script cleanup

--- Pacman/Assets/Scripts/Ghost.cs
+++ Pacman/Assets/Scripts/Ghost.cs
@@ -1,9 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.UIElements;
 
 public class Ghost : MonoBehaviour
 {
@@ -14,24 +11,35 @@ public class Ghost : MonoBehaviour
         scatter
     }
 
+    public enum Type
+    {
+        blinky,
+        clyde,
+        pinky,
+        inky
+    }
+
     [SerializeField] float moveSpeed = 3;
 
+    // caches
     Transform trans;
     Rigidbody2D rb;
+    [HideInInspector] public Node currentNode;
+    [HideInInspector] public Node targetNode;
+    [HideInInspector] public Vector2 currentDirection;
 
-    public Node currentNode;
-    public Node targetNode;
-    public Vector2 currentDirection;
+    // state
+    [HideInInspector] public State currentState;
+    [HideInInspector] public Vector2 currentPos;
 
     // values to keep track of the movement from the current to the target node
     float distanceFromCurrent = 0;
     float distanceToTarget = 0;
 
-    public State currentState;
-    public Vector2 currentPos;
-
-    public Pacman pacman;
-
+    /// <summary>
+    /// Sets up the ghost at the node specified
+    /// </summary>
+    /// <param name="startingnode">The node to start at</param>
     public void Init(Node startingnode)
     {
         trans = transform;
@@ -44,8 +52,6 @@ public class Ghost : MonoBehaviour
         rb.MovePosition(currentPos);
 
         currentState = State.chase;
-
-        pacman = GameLogic.instance.pacman;
 
         MoveToNextNode();
     }
@@ -60,6 +66,7 @@ public class Ghost : MonoBehaviour
 
             rb.MovePosition(currentPos);
 
+            // if we have reached the target node, pick a new one
             if (progress01 == 1)
             {
                 currentNode = targetNode;
@@ -68,20 +75,36 @@ public class Ghost : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Picks and then instructs the ghost to move to a new node
+    /// </summary>
     void MoveToNextNode()
     {
-        Node next = ChooseNextNode(ref currentDirection);
+        Vector2 nextDirection = Vector2.zero;
+        Node next = ChooseNextNode(ref nextDirection);
+
         targetNode = next;
+        currentDirection = nextDirection;
 
         distanceToTarget = Vector2.Distance(currentNode.pos, targetNode.pos);       // first calculate the distance required
         distanceFromCurrent = 0;
     }
 
+    /// <summary>
+    /// Gets the position this ghost wants to move to, this is different depending on the ghost type
+    /// </summary>
+    /// <returns>The position to target</returns>
     Vector2 GetTargetTile()
     {
-        return pacman.currentPos;
+        return GameLogic.instance.pacman.currentPos;
     }
 
+    /// <summary>
+    /// Picks the next node for the ghost to move
+    /// This is called only on init and when the ghost has just reached a node
+    /// </summary>
+    /// <param name="nextDir">A reference to the next direction that the ghost going to move</param>
+    /// <returns>The next node that the ghost is moving to</returns>
     Node ChooseNextNode(ref Vector2 nextDir)
     {
         Vector2 targetTile = GetTargetTile();
@@ -99,12 +122,17 @@ public class Ghost : MonoBehaviour
             }
         }
 
+        // declare some values to return
         Node nextNode = null;
         Vector2 nextDirection = Vector2.zero;
 
+        // if there is only 1 node to move to, just return that
         if (validNodes.Count == 1)
+        {
             nextNode = validNodes[0];
-        else // > 1
+            nextDirection = validDirections[0];
+        }
+        else // choose the node which is closest to the target (straight line distance) (maybe in the future calculate the actual distance?)
         {
             float leastDist = float.MaxValue;
             for (int i = 0; i < validNodes.Count; i++)
@@ -119,8 +147,7 @@ public class Ghost : MonoBehaviour
             }
         }
 
-        nextDir = nextDirection;
-
-        return nextNode;
+        nextDir = nextDirection;    // "return" the next direction
+        return nextNode;            // return the next node
     }
 }
