b3f12f3e0a943afc2b4f78ee3e804034db9a9824
Errol <errolflynn8@hotmail.com>
2020-09-12 16:20:08 +1000

    Player pref tool

---
+++ Pacman/Assets/Scripts/MenuTools.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MenuTools
+{
+	[MenuItem("Tools/Nuke Player Prefs")]
+	static void DeletePlayerPrefs()
+	{
+		PlayerPrefs.DeleteAll();
+		Debug.Log("Nuking prefs");
+	}
+}
